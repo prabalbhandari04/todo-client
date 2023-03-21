c932de08c13cd9ee4beccf62a9be66ddf4332ffb
Prabal Bhandari <54536906+prabalbhandari04@users.noreply.github.com>
2023-03-21 15:13:55 +0545

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+
+
+const Header = () => {
+
+    return (
+        <header>
+            <h1>To Do</h1>
+            <h4>Click on the task to update its status to done!</h4>
+        </header>
+    )
+}
+
+export default Header;
